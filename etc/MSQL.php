f694f3f3161581b1258be1fa7879acce20c8a928
popytka008 <sasha_1834b@mail.ru>
2015-09-30 21:03:46 +0300

    подправил шаблоны html

--- etc/MSQL.php
+++ etc/MSQL.php
@@ -32,25 +32,17 @@ class MSQL
     $instance = new MSQL();
     return $instance;
   }
-
   public function __destruct()
   {
-    //print_r($this->_connection);
     mysql_close($this->_connection);
   }
 
   public function selectLoginPassword(array $argv) //select(array('`users`', '`login`', $login, '`password`', $password)))
   {
     $resource = mysql_query("SELECT * FROM {$argv[0]} WHERE {$argv[1]} = '{$argv[2]}' AND {$argv[3]} = '{$argv[4]}'");
-
     if (!($result = mysql_fetch_assoc($resource))) {
       $this->error = mysql_error();
     }
-
-    //print_r("SELECT * FROM {$argv[0]} WHERE {$argv[1]} = '{$argv[2]}' AND {$argv[3]} = '{$argv[4]}'".PHP_EOL.'<br/>');
-    //print_r('строк: '.mysql_num_rows($resource).PHP_EOL.'<br/>');
-
-
     return $result;
   }
 
@@ -62,45 +54,25 @@ class MSQL
       $this->error = mysql_error();
     }
 
-    //print_r("SELECT * FROM {$argv[0]} WHERE {$argv[1]} = '{$argv[2]}'".PHP_EOL.'<br/>');
-    //print_r('строк: '.mysql_num_rows($resource).PHP_EOL.'<br/>');
-
-
     return $result;
   }
-
   public function delete(array $argv)  //delete(array('`online`', '`sid`', $sid));
   {
     $resource = mysql_query("DELETE FROM {$argv[0]} WHERE {$argv[1]} = '{$argv[2]}'");
 
-    //print_r("DELETE FROM {$argv[0]} WHERE {$argv[1]} = '{$argv[2]}'".PHP_EOL.'<br/>');
-    //var_export($resource);echo PHP_EOL.'<br/>';
-    //
-    //print_r('строк: '.mysql_affected_rows()); echo PHP_EOL.'<br/>';
     return mysql_affected_rows();
   }
-
   public function update(array $argv) //update(array('`online`', '`time_last`', time(),'`sid`', $sid))
   {
     $resource = mysql_query("UPDATE {$argv[0]} SET {$argv[1]} = {$argv[2]} WHERE {$argv[3]} = '{$argv[4]}'");
 
-    //print_r("UPDATE {$argv[0]} SET {$argv[1]} = {$argv[2]} WHERE {$argv[3]} = '{$argv[4]}'".PHP_EOL.'<br/>');
-    //var_export($resource);echo PHP_EOL.'<br/>';
-    //print_r('строк: '.mysql_affected_rows());echo PHP_EOL.'<br/>';
-
     return mysql_affected_rows();
   }
-
   public function insert($table, array $argv) //insert('`online`', array('`sid`', $sid, '`time_last`', time(), '`id_user`', $uid));
   {
     $resource = mysql_query("INSERT INTO {$table}({$argv[0]}, {$argv[2]}, {$argv[4]}) VALUES ('{$argv[1]}', {$argv[3]}, {$argv[5]})");
 
-    //print_r("INSERT INTO {$table}({$argv[0]}, {$argv[2]}, {$argv[4]}) VALUES ('{$argv[1]}', {$argv[3]}, {$argv[5]})".PHP_EOL.'<br/>');
-    //var_export($resource);echo PHP_EOL.'<br/>';
-    //print_r('строк: '.mysql_affected_rows());echo PHP_EOL.'<br/>';
-
     return mysql_affected_rows();
-    //return false;
   }
 
   public function getCount(array $argv)/*array('`online`', '`sid`', $sid))*/
@@ -110,9 +82,6 @@ class MSQL
     if ($result = mysql_fetch_row($resource)){
       $count = ((int)$result[0] > 0);
     }
-
-    //print_r("SELECT COUNT(*) FROM {$argv[0]} WHERE {$argv[1]} = '{$argv[2]}'".PHP_EOL.'<br/>');
-    //print_r('строк: '.mysql_num_rows($resource).PHP_EOL.'<br/>');
 
     return $count;
   }
